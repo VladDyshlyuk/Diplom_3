521cc1f221085477e7e8add39072afbc71d2dd10
VladDyshlyuk <vladdyshlyuk@yandex.ru>
2025-05-18 15:43:38 +0700

    added dependencies and tests for register page

---
+++ src/main/java/org/example/utils/Utils.java
@@ -0,0 +1,33 @@
+package org.example.utils;
+
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class Utils {
+
+    public static String randomString() {
+        return randomString(10);
+    }
+
+    public static String randomString(int length) {
+        Random random = new Random();
+        int leftLimit = 97;
+        int rightLimit = 122;
+        StringBuilder buffer = new StringBuilder(length);
+
+        for(int i = 0; i < length; ++i) {
+            int randomLimitedInt = leftLimit + (int)(random.nextFloat() * (float)(rightLimit - leftLimit + 1));
+            buffer.append(Character.toChars(randomLimitedInt));
+        }
+
+        return buffer.toString();
+    }
+
+    public static String randomEmail(){
+        return randomString(10) + "@gmail.com";
+    }
+
+    public static Integer randomNumber(Integer min, Integer max){
+        return ThreadLocalRandom.current().nextInt(min, max + 1);
+    }
+}
